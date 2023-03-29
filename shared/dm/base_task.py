e759df96f2dce8e45e483cf3bd942e3ad60cee4b
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-03-29 11:19:04 +0200

    TASK: normalize embeddings to unit vectors by default

--- shared/dm/base_task.py
+++ shared/dm/base_task.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import numpy as np
 import pandas as pd
 from utils.enums import TaskMode
 from utils.report import TaskReport
@@ -8,7 +9,7 @@ from utils.dataset import Dataset
 class BaseTask(ABC):
     def __init__(self, config: dict, entity_embeddings: pd.DataFrame, dataset: Dataset, report: TaskReport):
         self.config = config
-        self.entity_embeddings = entity_embeddings
+        self.entity_embeddings = entity_embeddings.apply(lambda x: x / np.linalg.norm(x, ord=1), axis=1)  # unit vectors
         self.dataset = dataset
         self.report = report
 
