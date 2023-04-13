426155358b6c9f03c175f8d176b787a5e6be4065
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-04-13 13:42:04 +0200

    TASK: Add DBpedia2016 as example KG

---
+++ kg/dbpedia2016/download_dbpedia.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget http://downloads.dbpedia.org/2016-10/dbpedia_2016-10.nt
+wget http://downloads.dbpedia.org/2016-10/core-i18n/en/instance_types_en.ttl.bz2
+wget http://downloads.dbpedia.org/2016-10/core-i18n/en/mappingbased_objects_en.ttl.bz2
