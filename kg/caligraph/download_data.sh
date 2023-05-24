d8d0e1f10282cd98e1b7aa3a8e4059e342d3b0fd
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-05-24 16:42:30 +0200

    TASK: Add caligraph 3.1

---
+++ kg/caligraph/download_data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir -p data
+cd data
+wget https://zenodo.org/record/7962777/files/caligraph-ontology.nt.bz2
+wget https://zenodo.org/record/7962777/files/caligraph-instances_types.nt.bz2
+wget https://zenodo.org/record/7962777/files/caligraph-instances_relations.nt.bz2
