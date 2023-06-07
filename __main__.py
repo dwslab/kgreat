011ec56978dfe36cf59a7f038353693baac72590
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-06-07 09:24:22 +0200

    REFACTOR: change order of params

--- __main__.py
+++ __main__.py
@@ -4,18 +4,18 @@ from util import get_image_name, load_kg_config, get_one_step_per_type, trigger_
 from prepare_mapping import collect_entities_to_map
 
 
-def process_action(kg_name: str, stage: str, action: str, steps: List[str], container_manager: str):
+def process_action(kg_name: str, action: str, stage: str, steps: List[str], container_manager: str):
     if stage == 'mapping':
-        _perform_mapping_action(kg_name, stage, action, steps, container_manager)
+        _perform_mapping_action(kg_name, action, stage, steps, container_manager)
     elif stage == 'preprocessing/embeddings':
-        _perform_embedding_action(kg_name, stage, action, steps, container_manager)
+        _perform_embedding_action(kg_name, action, stage, steps, container_manager)
     elif stage == 'tasks':
-        _perform_task_action(kg_name, stage, action, steps, container_manager)
+        _perform_task_action(kg_name, action, stage, steps, container_manager)
     else:
         raise NotImplementedError(f'Stage not implemented: {stage}')
 
 
-def _perform_mapping_action(kg_name: str, stage: str, action: str, steps: List[str], container_manager: str):
+def _perform_mapping_action(kg_name: str, action: str, stage: str, steps: List[str], container_manager: str):
     if action == 'prepare':
         collect_entities_to_map(kg_name, container_manager)
         return
@@ -30,12 +30,12 @@ def _perform_mapping_action(kg_name: str, stage: str, action: str, steps: List[s
         _perform_action(container_manager, action, image_name, path_to_dockerfile, kg_name, mapper_id)
 
 
-def _perform_embedding_action(kg_name: str, stage: str, action: str, steps: List[str], container_manager: str):
+def _perform_embedding_action(kg_name: str, action: str, stage: str, steps: List[str], container_manager: str):
     path_to_dockerfile = './shared/preprocessing/embeddings/Dockerfile'
     _perform_action(container_manager, action, get_image_name(stage), path_to_dockerfile, kg_name)
 
 
-def _perform_task_action(kg_name: str, stage: str, action: str, steps: List[str], container_manager: str):
+def _perform_task_action(kg_name: str, action: str, stage: str, steps: List[str], container_manager: str):
     task_config = load_kg_config(kg_name)['tasks']
     if not steps:
         # handling one of every step type (= image) is enough if we do not run them
@@ -83,9 +83,9 @@ def _action_run(container_manager: str, image_name: str, kg_name: str, step_id: 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Manage and run images for preprocessing or tasks.')
     parser.add_argument('kg_name', type=str, help='Name of the knowledge graph to use')
-    parser.add_argument('stage', type=str, help='Apply action to this stage', choices=['mapping', 'preprocessing/embeddings', 'tasks'])
     parser.add_argument('action', type=str, help='Action to perform', choices=['build', 'push', 'pull', 'prepare', 'run'])
+    parser.add_argument('stage', type=str, help='Apply action to this stage', choices=['mapping', 'preprocessing/embeddings', 'tasks'])
     parser.add_argument('-s', '--step', type=str, nargs='*', help='ID of step(s) to perform action on (default is: all steps of stage)')
     parser.add_argument('-c', '--container_manager', type=str, help='Name of container manager', choices=['docker', 'podman'], default='docker')
     args = parser.parse_args()
-    process_action(args.kg_name, args.stage, args.action, args.step, args.container_manager)
+    process_action(args.kg_name, args.action, args.stage, args.step, args.container_manager)
