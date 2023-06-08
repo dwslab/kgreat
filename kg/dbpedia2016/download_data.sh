ce524fec5a35873abef05931a7c6a331fc74323b
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-06-08 11:03:05 +0200

    REFACTOR: More generic usage of known entities file

--- kg/dbpedia2016/download_data.sh
+++ kg/dbpedia2016/download_data.sh
@@ -4,3 +4,4 @@ cd data
 wget http://downloads.dbpedia.org/2016-10/dbpedia_2016-10.nt
 wget http://downloads.dbpedia.org/2016-10/core-i18n/en/instance_types_en.ttl.bz2
 wget http://downloads.dbpedia.org/2016-10/core-i18n/en/mappingbased_objects_en.ttl.bz2
+wget http://downloads.dbpedia.org/2016-10/core-i18n/en/labels_en.ttl.bz2
