9e59bdf5bf8f389af079cff45df65011f3842db5
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-06-12 15:49:27 +0200

    TASK: Implement ANN for semantic analogies

--- shared/dm/semanticanalogies_task.py
+++ shared/dm/semanticanalogies_task.py
@@ -2,6 +2,7 @@ from typing import List
 from collections import defaultdict
 import numpy as np
 import pandas as pd
+import hnswlib
 from utils.logging import get_logger
 from utils.enums import TaskMode, EntityMode
 from utils.dataset import SemanticAnalogiesDataset
@@ -21,10 +22,11 @@ class SemanticAnalogiesTask(BaseTask):
         for embedding_type in self.embedding_models:
             correct_predictions_by_k = defaultdict(int)
             entity_embeddings = self.load_entity_embeddings(embedding_type)
+            entity_embedding_index = self._build_ann_index(entity_embeddings.to_numpy(), 400, 64, 50)
             get_logger().debug(f'Evaluating semantic analogies via cosine distance for embedding type {embedding_type}')
             for analogy_set in mapped_analogy_sets.itertuples(index=False):
                 a, b, c, d = (entity_embeddings.index.get_loc(ent) for ent in analogy_set)  # retrieve ent indices
-                d_pred = self._predict_analogy(entity_embeddings, a, b, c)
+                d_pred = self._predict_analogy(entity_embeddings, entity_embedding_index, max(self.TOP_K), a, b, c)
                 for k in self.TOP_K:
                     if d in d_pred[:k]:
                         correct_predictions_by_k[k] += 1
@@ -39,7 +41,16 @@ class SemanticAnalogiesTask(BaseTask):
                     self.report.add_result(entity_mode, 'Cosine similarity', {'top_k': k}, embedding_type, 'Accuracy', score)
 
     @staticmethod
-    def _predict_analogy(entity_embeddings: pd.DataFrame, a: int, b: int, c: int) -> List[int]:
+    def _build_ann_index(embeddings: np.ndarray, ef_construction: int, M: int, ef: int) -> hnswlib.Index:
+        get_logger().debug('Building ANN index..')
+        index = hnswlib.Index(space='ip', dim=embeddings.shape[-1])
+        index.init_index(max_elements=len(embeddings), ef_construction=ef_construction, M=M)
+        index.add_items(embeddings, list(range(len(embeddings))))
+        index.set_ef(ef)
+        return index
+
+    @staticmethod
+    def _predict_analogy(entity_embeddings: pd.DataFrame, entity_embedding_index: hnswlib.Index, max_k: int, a: int, b: int, c: int) -> List[int]:
         d_vec = entity_embeddings.iloc[b, :] - entity_embeddings.iloc[a, :] + entity_embeddings.iloc[c, :]
-        similarity_to_d = np.dot(entity_embeddings, d_vec)
-        return [ent_idx for ent_idx in np.argsort(-similarity_to_d) if ent_idx not in {a, b, c}]
+        indices, _ = entity_embedding_index.knn_query(d_vec.to_numpy().reshape(1, -1), k=max_k+3)
+        return [ent_idx for ent_idx in indices.flatten() if ent_idx not in {a, b, c}]
