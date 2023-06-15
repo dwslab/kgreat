21e056eb9257a78cc1e9b25449d90fc8a84d6c3c
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-06-15 16:01:28 +0200

    TASK: DBpedia sameas links for wikidata

--- kg/wikidata/download_data.sh
+++ kg/wikidata/download_data.sh
@@ -2,5 +2,7 @@
 mkdir -p data && cd data
 wget https://data.dws.informatik.uni-mannheim.de/kgreat/wikidata_labels.nt
 wget https://data.dws.informatik.uni-mannheim.de/kgreat/wikidata_sameas.nt
+wget -O wikidata_sameas_dbpedia.nt.bz2 https://downloads.dbpedia.org/repo/dbpedia/wikidata/sameas-all-wikis/2022.12.01/sameas-all-wikis.ttl.bz2 && bunzip2 wikidata_sameas_dbpedia.nt.bz2
+sed -i 's|wikidata.dbpedia.org/resource|wikidata.org/entity|g' wikidata_sameas_dbpedia.nt
 cd .. && mkdir -p embeddings && cd embeddings
 wget -O TransE_l1.tsv.gz https://dl.fbaipublicfiles.com/torchbiggraph/wikidata_translation_v1.tsv.gz && gzip -d TransE_l1.tsv.gz
