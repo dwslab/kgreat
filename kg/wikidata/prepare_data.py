75812b55b5270ae44fe0d25db2d9dcebb0b7b81e
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-06-15 13:33:40 +0200

    REFACTOR: More efficient wikidata parsing method

--- kg/wikidata/prepare_data.py
+++ kg/wikidata/prepare_data.py
@@ -6,7 +6,14 @@ EMBEDDING_FILE = Path('./embeddings/TransE_l1.tsv')
 if not EMBEDDING_FILE.is_file():
     raise FileNotFoundError('Embedding file not found. Run the download script first!')
 
-df = pd.read_csv(EMBEDDING_FILE, sep='\t', header=None, skiprows=1)
-df = df[(df[0].str.startswith('<')) & (df[0].str.endswith('>'))]
-df[0] = df[0].str.slice(start=1, stop=-1)
-df.to_csv(EMBEDDING_FILE, sep='\t', index=False)
+data = []
+with open(EMBEDDING_FILE) as f:
+    for line in f:
+        if not line.startswith('<'):
+            continue
+        item = line.split('\t')
+        if not item[0].endswith('>'):
+            continue
+        item[0] = item[0][1:-1]
+        data.append(item)
+pd.DataFrame(data=data, columns=['ent'] + list(range(200))).to_csv(EMBEDDING_FILE, sep='\t', index=False, header=False)
