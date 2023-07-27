45bb7cd7c7b964c20e5dc6f8be0a33b8ba55b3e4
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-07-27 15:30:18 +0200

    TASK: make sure log folder exists

--- shared/dm/utils/logger.py
+++ shared/dm/utils/logger.py
@@ -1,5 +1,6 @@
-import logging
 import datetime
+from pathlib import Path
+import logging
 
 
 LOGGER_NAME = None
@@ -9,8 +10,10 @@ def init_logger(log_level: str, run_id: str, task_id: str):
     global LOGGER_NAME
     LOGGER_NAME = f'task-{task_id}'
     log_time = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-    log_filepath = f'kg/log/{run_id}/{log_time}_{LOGGER_NAME}.log'
-    log_handler = logging.FileHandler(log_filepath, 'a', 'utf-8')
+    log_folder = Path(f'kg/log/{run_id}')
+    log_folder.mkdir(parents=True, exist_ok=True)  # make sure log folder exists
+    log_file = log_folder / f'kg/log/{run_id}/{log_time}_{LOGGER_NAME}.log'
+    log_handler = logging.FileHandler(log_file, 'a', 'utf-8')
     log_handler.setFormatter(logging.Formatter('%(asctime)s|%(levelname)s|%(module)s->%(funcName)s: %(message)s'))
     log_handler.setLevel(log_level)
     logger = get_logger()
