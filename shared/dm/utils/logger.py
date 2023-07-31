cffd858e2c09513c2cc1642fac33fb339c7a120b
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-07-31 15:17:41 +0200

    FIX: Fix logging in tasks

--- shared/dm/utils/logger.py
+++ shared/dm/utils/logger.py
@@ -12,7 +12,7 @@ def init_logger(log_level: str, run_id: str, task_id: str):
     log_time = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
     log_folder = Path(f'kg/log/{run_id}')
     log_folder.mkdir(parents=True, exist_ok=True)  # make sure log folder exists
-    log_file = log_folder / f'kg/log/{run_id}/{log_time}_{LOGGER_NAME}.log'
+    log_file = log_folder / f'{log_time}_{LOGGER_NAME}.log'
     log_handler = logging.FileHandler(log_file, 'a', 'utf-8')
     log_handler.setFormatter(logging.Formatter('%(asctime)s|%(levelname)s|%(module)s->%(funcName)s: %(message)s'))
     log_handler.setLevel(log_level)
