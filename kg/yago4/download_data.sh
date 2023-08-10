4edd82e4b39617c42132113198f8be428d078ef2
Nicolas Heist <nico@informatik.uni-mannheim.de>
2023-08-10 17:06:11 +0200

    TASK: Add YAGO4

---
+++ kg/yago4/download_data.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir -p data && cd data
+
+FILES=('yago-wd-class.nt.gz' 'yago-wd-schema.nt.gz' 'yago-wd-full-types.nt.gz' 'yago-wd-facts.nt.gz' 'yago-wd-labels.nt.gz' 'yago-wd-sameAs.nt.gz')
+for file in "${FILES[@]}"
+do
+  wget https://yago-knowledge.org/data/yago4/en/2020-02-24/$file && gunzip $file
+done
